fd31edffbc05b7735ab2192bccffca13126fe795
blacktop <blacktop@users.noreply.github.com>
2016-12-29 23:32:22 -0700

    playing with fields

--- scripts/add-template.sh
+++ scripts/add-template.sh
@@ -2,11 +2,16 @@
 
 set -e
 
+echo "===> Delete old logs"
+http DELETE 'http://localhost:9200/filebeat-*'
+
 echo "===> Add template"
-http 'localhost:9200/_template/brobeat-*' < brobeat.template.json
+http 'localhost:9200/_template/brobeat' < brobeat.template.json
 
 echo "===> Add index-pattern"
 http 'localhost:9200/.kibana/index-pattern/brobeat-*' < _meta/kibana/index-pattern/brobeat.json
 
 echo "===> Get indices and _types"
 http 'localhost:9200/_mapping' | jq 'to_entries | .[] | {(.key): .value.mappings | keys}'
+
+# http POST 'http://localhost:9200/brobeat-*/_refresh'
