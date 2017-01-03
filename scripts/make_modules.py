f9fccddf3d10d37bba54310ae22734318704c2b2
blacktop <blacktop@users.noreply.github.com>
2017-01-02 22:28:39 -0700

    working on modules

--- scripts/make_modules.py
+++ scripts/make_modules.py
@@ -16,7 +16,7 @@ with open('../logstash/bro.conf', 'w') as lsfile:
 # create module manifests
 template = env.get_template('module/manifest.yml')
 for log in bro_logs['logs']:
-    module_dir = os.path.join('../module/bro', log['log_type'])
+    module_dir = os.path.join('../modules', log['log_type'])
     if log.get('pattern', None):
         if not os.path.isdir(module_dir):
             os.makedirs(module_dir, 0755)
@@ -26,7 +26,7 @@ for log in bro_logs['logs']:
 # create module config
 template = env.get_template('module/config/config.yml')
 for log in bro_logs['logs']:
-    config_dir = os.path.join('../module/bro', log['log_type'], 'config')
+    config_dir = os.path.join('../modules', log['log_type'], 'config')
     if log.get('pattern', None):
         if not os.path.isdir(config_dir):
             os.makedirs(config_dir, 0755)
@@ -36,7 +36,7 @@ for log in bro_logs['logs']:
 # create ingest pipelines
 template = env.get_template('module/ingest/pipeline.json')
 for log in bro_logs['logs']:
-    ingest_dir = os.path.join('../module/bro', log['log_type'], 'ingest')
+    ingest_dir = os.path.join('../modules', log['log_type'], 'ingest')
     if log.get('pattern', None):
         if not os.path.isdir(ingest_dir):
             os.makedirs(ingest_dir, 0755)
@@ -48,7 +48,7 @@ for log in bro_logs['logs']:
 # create _meta fields
 template = env.get_template('module/_meta/fields.yml')
 for log in bro_logs['logs']:
-    meta_dir = os.path.join('../module/bro', log['log_type'], '_meta')
+    meta_dir = os.path.join('../modules', log['log_type'], '_meta')
     if log.get('pattern', None):
         if not os.path.isdir(meta_dir):
             os.makedirs(meta_dir, 0755)
